403126d43c67da95ea941133973bfe317c5c287f
iber <iberson-silva@outlook.com>
2024-05-14 17:01:07 -0500

    comments

--- src/product/index.js
+++ src/product/index.js
@@ -9,14 +9,19 @@ const port = 8080
 const { productModel } = require('./models');
 app.get('/', (req, res) => { res.send("I am alive product"); })
 
+// ver u obtener producto
 app.get('/product', async(req, res)=>{
   const product = await productModel.find({});
   res.json( product );
 });
+
+// ver u obtener producto por DNI
 app.get('/product/:dni', async(req, res)=>{
   const produc = await productModel.find({dni:req.params.dni});
   res.json( produc );
 });
+
+// crear producto
 app.post('/product', async(req, res)=>{
   try {
     const codeProduct = req.body.codeProduct;
